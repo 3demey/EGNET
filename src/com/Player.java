5256b61c3a7c8526b5bfe134476dda972ff2bfbc
GuyMenashe3107 <guymenashe31@gmail.com>
2020-01-13 14:16:40 +0200

    hi

--- src/com/Player.java
+++ src/com/Player.java
@@ -8,21 +8,30 @@ public class Player {
     //NEED TO SWITCH ALL prints TO Strings, so we can send to dealer.
     //NEED A MAIN MENU FOR OUR PLAYER TO CHOOSE HIS NEXT MOVE. need a main class in general.
     //Implementation for unwritten methods.
-    public boolean request() {}
-    public void betMsg(int bet,DataInputStream consoleInput, PrintStream toDealerOutputStream) throws IOException
-    {		// Queries desired betting amount from player and sends it to the dealer.
-        System.out.println("================================");
-        System.out.print("Please Enter the amount you are willing to bet on:  ");
-        bet = consoleInput.readInt();
-        System.out.println("================================");
-        toDealerOutputStream.print(bet);
-    }
-    public void playAgainMsg() {}
-    public void viewStatus(PrintStream toDealerOutputStream)
-    { // Allows player to request from the dealer to view his current game status.
 
-    }
-    public void quit() {}
+
+//    public int betMsg(int bet,DataInputStream consoleInput, PrintStream toDealerOutputStream)
+//    {		// Queries desired betting amount from player and sends it to the dealer.
+//        //should change it to return string? no, send it to the dealer
+//        System.out.println("================================");
+//        System.out.print("Please Enter the amount you are willing to bet on:  ");
+//        bet = consoleInput.readInt();
+//        System.out.println("================================");
+//        toDealerOutputStream.print(bet);
+//    }
+
+//    public int answerToInt(String answer)
+//    {
+//        //gets the choice if he wants to continue or not, and returns int, in main we will return to server.
+//        System.out.println("Enter yes for surrender and no to continue");
+//        int srndr ;
+//        if(answer =="yes")
+//            srndr = 1;
+//        else {
+//            srndr = 0;
+//        }
+//        return srndr;
+//    }
 
     public static void main(String[] args) {
         Socket socket = null;
@@ -30,17 +39,26 @@ public class Player {
         DataInputStream consoleInput;
         PrintStream toDealerOutputStream;
         String line = "";
+        int answer;
         try{
             socket = new Socket("localhost",7000);
             System.out.println(new Date() + "Connected to server- " + socket.getLocalAddress() + ":" + socket.getLocalPort());
             fromDealerInputStream = new DataInputStream(socket.getInputStream());
             toDealerOutputStream = new PrintStream(socket.getOutputStream());
             consoleInput = new DataInputStream(System.in);
+            //gets the messege from server adn prints it for him
+            line = fromDealerInputStream.readLine();
+            while(!(line.equals("PIPIKAKI"))){
+                System.out.println(line);
+                answer = consoleInput.readInt();
+                toDealerOutputStream.print(answer);
+                line = fromDealerInputStream.readLine();
+            }
         } catch(Exception e) {System.err.println(e);}
         finally {
             try{
+                System.out.println(line);
                 socket.close();
-                System.out.println("GOODBYE, hope you had fun!");
             } catch(IOException e) {}
         }
     }
