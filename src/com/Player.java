91e5bcbedab7c474deb1b2dc0e24dce041869df8
3demey <edenmeyer@gmail.com>
2020-01-14 10:59:03 +0200

    almost final

--- src/com/Player.java
+++ src/com/Player.java
@@ -6,17 +6,6 @@ import java.util.*;
 
 public class Player {
 
-//    public int betMsg(int bet,DataInputStream consoleInput, PrintStream toDealerOutputStream)
-//    {		// Queries desired betting amount from player and sends it to the dealer.
-//        //should change it to return string? no, send it to the dealer
-//        System.out.println("================================");
-//        System.out.print("Please Enter the amount you are willing to bet on:  ");
-//        bet = consoleInput.readInt();
-//        System.out.println("================================");
-//        toDealerOutputStream.print(bet);
-//    }
-
-
     public static void main(String[] args) {
         Socket socket = null;
         DataInputStream fromDealerInputStream;
@@ -27,11 +16,11 @@ public class Player {
         int answer;
         try{
             socket = new Socket("localhost",2000);
-            System.out.println(new Date() + "Connected to server- " + socket.getLocalAddress() + ":" + socket.getLocalPort());
+            System.out.println(new Date() + "Connected to server - " + socket.getLocalAddress() + ":" + socket.getLocalPort());
             fromDealerInputStream = new DataInputStream(socket.getInputStream());
             toDealerOutputStream = new PrintStream(socket.getOutputStream());
             consoleInput = new DataInputStream(System.in);
-            //gets the messege from server adn prints it for him
+            // Gets the message from server and prints it for him
             initLine = fromDealerInputStream.readLine();
             line = initLine.replace('#', '\n');
             while(!(line.equals("PIPIKAKI"))){
@@ -44,9 +33,9 @@ public class Player {
         } catch(Exception e) {System.err.println(e);}
         finally {
             try{
-                System.out.println(line);
                 socket.close();
+                System.out.println("Client disconnected");
             } catch(IOException e) {}
         }
-    }
-}
+    } // main
+} // class - Dealer
